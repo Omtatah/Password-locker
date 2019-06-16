c85612cf1b97e6f07cd00c270d37dbeac05187dd
Omtatah <murtallahomtatah@gmail.com>
2019-06-16 14:11:34 +0300

    add User 3 class methods to find account, display account and remove account

--- user.py
+++ user.py
@@ -16,3 +16,28 @@ class User:
         self.first_name = first_name
         self.second_name = second_name
         self.password = password
+
+    @classmethod
+    def display_users(cls):
+        '''
+        method to display all the users
+        '''
+        return cls.user_list
+    @classmethod
+    def user_exist(cls,password):
+        '''
+        method to check if a user and
+        their details exist
+        '''
+        for user in cls.user_list:
+            if  user.password == password:
+                return True
+        return False
+    @classmethod
+    def find_account(cls,password2):
+        '''
+        method that finds an account by its name
+        '''
+        for user in cls.user_list:
+            if user.password == password2:
+                return user
