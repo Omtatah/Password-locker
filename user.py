596c42eefa70b3536e101d69c7537114b1bdec15
Omtatah <murtallahomtatah@gmail.com>
2019-06-16 14:09:54 +0300

    add a User class

---
+++ user.py
@@ -0,0 +1,18 @@
+from credential import Credential
+class User: 
+    '''
+    class that generates new instances of users accounts
+    '''
+    user_list = []#user list
+    def save_user(self):
+        '''
+        save_user method saves users
+        '''
+        User.user_list.append(self)
+    def __init__(self,first_name,second_name,password):
+
+       #create new user instance
+
+        self.first_name = first_name
+        self.second_name = second_name
+        self.password = password
