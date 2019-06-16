fced3a5eae30dcf13c921e0f143c855ee31cc894
Omtatah <murtallahomtatah@gmail.com>
2019-06-16 14:46:47 +0300

    define functions to save users, display users, create user, check_existing_users, delete credentials, save credentials etc

--- run.py
+++ run.py
@@ -4,3 +4,51 @@ import random
 import getpass
 import pyperclip
 from credential import Credential
+
+def create_credential(account_name,passkey):
+    '''
+    function to create a new credential
+    '''
+    new_credential = Credential(account_name,passkey)
+    return new_credential
+def save_credential(credential):
+    '''
+    function to save a credential
+    '''
+    credential.save_credential()
+def display_credentials():
+    '''
+    function that dispalys all credentials
+    '''
+    return Credential.display_credentials()
+def del_credential(credential):
+    '''
+    function to delete a credential
+    '''
+    credential.del_credential()
+def check_existing_user(password2):
+    '''
+    function to check that enable login authentification
+    '''
+    return User.user_exist(password2)
+def find_account(password2):
+    '''
+    function to find account by its name
+    '''
+    return User.find_account(password2)
+def create_user(f_name,s_name,password):
+    '''
+    function to create a new user
+    '''
+    new_user = User(f_name,s_name,password)
+    return new_user
+def save_users(user):
+    '''
+    function to save a user
+    '''
+    user.save_user()
+def display_users():
+    '''
+    function that dispalys all signed up users
+    '''
+    return User.display_users()
