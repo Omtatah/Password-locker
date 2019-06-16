28366bd850d69acd454ec04d35c0e7dffb097041
Omtatah <murtallahomtatah@gmail.com>
2019-06-16 13:51:39 +0300

    readjust the code structure and indentation to solve indentation error and make code test positively

--- credential_test.py
+++ credential_test.py
@@ -2,46 +2,53 @@ import unittest
 
 from credential import Credential
 
+
 class TestUser(unittest.TestCase):
     '''
     Test class that defines the test cases for user class
     behaviours
     '''
+
     def setUp(self):
         '''
         Setup method to run before each test cases
         '''
-        self.new_credential = Credential("twitter","tygr")
-         def test_init(self):
+        self.new_credential = Credential("twitter", "tygr")
+
+    def test_init(self):
         '''
         test_init checks if the object is initialised properly
         '''
-        self.assertEqual(self.new_credential.account_name,"twitter")
-        self.assertEqual(self.new_credential.passkey,"tygr")
-        
+        self.assertEqual(self.new_credential.account_name, "twitter")
+        self.assertEqual(self.new_credential.passkey, "tygr")
+
     def test_save_credential(self):
         '''
         test_save_credential tests if a new credential has been added to credentials
         list of a user
         '''
         self.new_credential.save_credential()
-        self.assertEqual(len(Credential.credential_list),1)
+        self.assertEqual(len(Credential.credential_list), 1)
+
     def test_display_credentials(self):
         '''
         test to display the credentials of a user
         '''
-        self.assertEqual(Credential.display_credentials(),Credential.credential_list)
+        self.assertEqual(Credential.display_credentials(),
+                         Credential.credential_list)
+
     def test_delete_credential(self):
         '''
         test_delete_credential to see if we can remove a 
         credential from credentials list
         '''
         self.new_credential.save_credential()
-        test_credential = Credential("test","0893uhjnv")
+        test_credential = Credential("test", "0893uhjnv")
         test_credential.save_credential()
 
         self.new_credential.delete_credential()
-        self.assertEqual(len(Credential.credential_list),1)
+        self.assertEqual(len(Credential.credential_list), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
