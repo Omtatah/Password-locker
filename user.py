85975b2eafdbab4f92a1bcc919bc7dc285870cf5
Omtatah <murtallahomtatah@gmail.com>
2019-06-16 14:12:32 +0300

    properly indent the user.py file

--- user.py
+++ user.py
@@ -1,17 +1,21 @@
 from credential import Credential
-class User: 
+
+
+class User:
     '''
     class that generates new instances of users accounts
     '''
-    user_list = []#user list
+    user_list = []  # user list
+
     def save_user(self):
         '''
         save_user method saves users
         '''
         User.user_list.append(self)
-    def __init__(self,first_name,second_name,password):
 
-       #create new user instance
+    def __init__(self, first_name, second_name, password):
+
+       # create new user instance
 
         self.first_name = first_name
         self.second_name = second_name
@@ -23,18 +27,20 @@ class User:
         method to display all the users
         '''
         return cls.user_list
+
     @classmethod
-    def user_exist(cls,password):
+    def user_exist(cls, password):
         '''
         method to check if a user and
         their details exist
         '''
         for user in cls.user_list:
-            if  user.password == password:
+            if user.password == password:
                 return True
         return False
+
     @classmethod
-    def find_account(cls,password2):
+    def find_account(cls, password2):
         '''
         method that finds an account by its name
         '''
