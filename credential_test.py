22742e3162ab95772aae96eb3d6587d0188f06e9
Omtatah <murtallahomtatah@gmail.com>
2019-06-16 13:29:06 +0300

    add TestUser subclass of the unittest.TestCase class

--- credential_test.py
+++ credential_test.py
@@ -1,3 +1,9 @@
 import unittest
 
 from credential import Credential
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines the test cases for user class
+    behaviours
+    '''
