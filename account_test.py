34a81c1ada43c0e38f6beaf197ac37c0828e0a8d
Omtatah <murtallahomtatah@gmail.com>
2019-06-16 14:36:11 +0300

    define test_Save_user, test_save_multiple, test_display_users, test user_exist and call the unittest function to allow for tests to run

--- account_test.py
+++ account_test.py
@@ -20,3 +20,38 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name,"Murtallah")
         self.assertEqual(self.new_user.second_name,"Omtatah")
         self.assertEqual(self.new_user.password,"tygr21")
+        
+        def test_save_user(self):
+        '''
+        test_save_user tests if a new user created is saved
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),3)
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user tests if many users can be saved
+        '''
+        self.new_user.save_user()
+        test_user = User("test","user","muk")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    def test_display_users(self):
+        '''
+        method that displays all thes signed up users
+        '''
+        self.assertEqual(User.display_users(),User.user_list)
+    def test_user_exist(self):
+        '''
+        test to check if a user exists in user list
+        '''
+        self.new_user.save_user()
+        test_user = User("test","sname","passw")
+        test_user.save_user()
+        user_exists =User.user_exist("test")
+    
+
+        self.assertTrue(user_exists)
+if __name__ == '__main__':
+    unittest.main()
